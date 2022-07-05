20977063929f607c752496e33baa4654483339cc
Todd Demone <todd.demone.dev@gmail.com>
2022-07-05 12:05:12 -0400

    change title of test app

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { App } from './App';
 
-test('renders alphabook string', () => {
+test('renders CRA17-SB-MUI string', () => {
   render(<App />);
-  const linkElement = screen.getByText(/alphabook/i);
+  const linkElement = screen.getByText(/cra17-sb-mui/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export function App() {
-  return <div>AlphaBook</div>;
+  return <div>CRA17-SB-MUI</div>;
 }
