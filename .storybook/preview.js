e8d360acac45856d27d353dde489330e2f520a83
Todd Demone <todd.demone.dev@gmail.com>
2022-07-05 12:42:25 -0400

    add basic storybook decorator for theme1 to .storybook/preview.js; add toolbar to storybook for theme changing

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,44 @@
+import { StyleProvider, ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import { theme1, theme2 } from "../src/theme";
+
+export const globalTypes = {
+  theme: {
+    name: "Theme",
+    description: "Global theme for components",
+    defaultValue: "theme1",
+    toolbar: {
+      icon: "circlehollow",
+      // Array of plain string values or MenuItem shape (see below)
+      items: ["theme1", "theme2"],
+      // Property that specifies if the name of the item will be displayed
+      showName: true,
+      // Change title based on selected value
+      dynamicTitle: true,
+    },
+  },
+};
+
+// const getTheme = (themeName) => {
+//   switch (themeName) {
+//     case "theme2":
+//       return theme2;
+//     default:
+//       return theme1;
+//   }
+// };
+
+const withThemeProvider = (Story, context) => {
+  return (
+    <ThemeProvider theme={theme1}>
+      <CssBaseline />
+      <Story {...context} />
+    </ThemeProvider>
+  );
+};
+
+export const decorators = [withThemeProvider];
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +47,4 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+};
--- src/theme.tsx
+++ src/theme.tsx
@@ -1,7 +1,6 @@
 import { red } from "@mui/material/colors";
 import { createTheme } from "@mui/material/styles";
 
-// A custom theme for this app
 export const theme1 = createTheme({
   palette: {
     primary: {
