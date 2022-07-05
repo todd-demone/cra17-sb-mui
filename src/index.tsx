1c0eca1ba0e5824f7c88f24e4c10a4f33957f92a
Todd Demone <todd.demone.dev@gmail.com>
2022-07-05 12:21:13 -0400

    add test MUI theme to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
 import { App } from "./App";
+import { theme1 } from "./theme";
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme1}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ src/theme.tsx
@@ -0,0 +1,37 @@
+import { red } from "@mui/material/colors";
+import { createTheme } from "@mui/material/styles";
+
+// A custom theme for this app
+export const theme1 = createTheme({
+  palette: {
+    primary: {
+      main: "#556cd6",
+    },
+    secondary: {
+      main: "#19857b",
+    },
+    error: {
+      main: red.A400,
+    },
+    background: {
+      default: "#fff",
+    },
+  },
+});
+
+export const theme2 = createTheme({
+  palette: {
+    primary: {
+      main: "#8862a1",
+    },
+    secondary: {
+      main: "#f6144a",
+    },
+    error: {
+      main: red.A400,
+    },
+    background: {
+      default: "#fff",
+    },
+  },
+});
